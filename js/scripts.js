26ab150e57e7a1f89f19a812652dbfd8c0bffd70
sabts <sabrinataraschi@gmail.com>
2025-03-24 11:59:33 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,21 @@ nameListCheeker();
 //2️⃣ Bego tiene un array con precios. Debe sumar todos los precios usando un bucle for y mostrar el total.
 //Ejemplo entrada: [23, 78, 45]
 //Ejemplo salida: Total: 146
-const sumPrices = (number,number,number) => {
-    number + number + number = result;
-    return result
-}
+const prices = [23, 78, 45];
+
 const priceSumTotal = () => {
-  for (price )
+  let sumtotal = 0;
+  for (let i = 0; i < prices.length; i++) {
+    sumtotal = sumtotal + prices[i];//tiene que ir dentro de {  }
+  }
+  console.log(sumtotal);
+};
+priceSumTotal();
+
+//3️⃣ Macarena quiere generar tres códigos de seguridad y almacenarlos en un array. Cada código debe ser un número aleatorio entre 1000 y 9999. Usa un bucle for para generar los códigos y luego mostrarlos.
+//Ejemplo salida: [4356, 7890, 1234]
+const threeDigitSecurityCode () => {
+
+
 }
+threeDigitSecurityCode([4356, 7890, 1234])
